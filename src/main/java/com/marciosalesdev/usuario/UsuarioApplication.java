c2ccd057ae494ffcc3bc93ef4371da4aa3f425b8
marcio.sales <marcioserrasales2014@gmail.com>
2026-05-21 15:14:11 -0300

    adicionado classes do  spring_security

--- src/main/java/com/marciosalesdev/usuario/UsuarioApplication.java
+++ src/main/java/com/marciosalesdev/usuario/UsuarioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsuarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsuarioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioApplication.class, args);
+    }
 
 }
