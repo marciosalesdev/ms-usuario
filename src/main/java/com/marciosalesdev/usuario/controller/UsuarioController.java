e70babe75abc3d9308ff362d8778eda8cabbd39f
marciosalesdev <marcioserrasales2014@gmail.com>
2026-05-22 01:14:50 -0300

    adicionado metodo que salva usuario

---
+++ src/main/java/com/marciosalesdev/usuario/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.marciosalesdev.usuario.controller;
+
+import com.marciosalesdev.usuario.business.UsuarioService;
+import com.marciosalesdev.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("usuario")
+@RestController
+public class UsuarioController {
+
+
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTO));
+    }
+}
--- src/main/java/com/marciosalesdev/usuario/infraestructure/entity/Endereco.java
+++ src/main/java/com/marciosalesdev/usuario/infraestructure/entity/Endereco.java
@@ -1,10 +1,7 @@
 package com.marciosalesdev.usuario.infraestructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "endereço")
+@Builder
 public class Endereco {
 
     @Id
